6c832d7f3b1d5e43bea7ebb8050f73e387b0ed54
Gabriel Gironda <gabriel.gironda@gmail.com>
2009-09-06 15:05:00 -0500

    Here comes the water.

---
+++ xulapp/defaults/preferences/prefs.js
@@ -0,0 +1,7 @@
+pref("toolkit.defaultChromeURI", "chrome://watertower/content/xul/boot.xul");
+pref("toolkit.defaultChromeFeatures", "chrome,menubar,status,resizable");
+pref("javascript.options.showInConsole", true);
+// TODO - only disable caching in development mode.
+pref("nglayout.debug.disable_xul_cache", true);
+pref("nglayout.debug.disable_xul_fastload", true);
+pref("browser.dom.window.dump.enabled", true);
